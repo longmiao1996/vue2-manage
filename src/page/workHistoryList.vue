0286d76a3f771afd2a1055c19d9dedf0145a8169
hide <miaol.fnst@cn.fujitsu.com>
2022-04-03 15:23:26 +0800

    显示job 内容完成

--- src/page/workHistoryList.vue
+++ src/page/workHistoryList.vue
@@ -11,18 +11,18 @@
                   width="100">
                 </el-table-column>
                 <el-table-column
-                  property="registe_time"
-                  label="注册日期"
+                  property="userName"
+                  label="姓名"
                   width="220">
                 </el-table-column>
                 <el-table-column
-                  property="username"
-                  label="用户姓名"
+                  property="jobContent"
+                  label="工作内容"
                   width="220">
                 </el-table-column>
                 <el-table-column
-                  property="city"
-                  label="注册地址">
+                  property="date"
+                  label="日期">
                 </el-table-column>
             </el-table>
             <div class="Pagination" style="text-align: left;margin-top: 10px;">
@@ -41,72 +41,61 @@
 
 <script>
     import headTop from '../components/headTop'
-    import {getUserList, getUserCount} from '@/api/getData'
+    import {getUserList, getUserCount, getJobHistory} from '@/api/getData'
     export default {
         data(){
             return {
-                tableData: [{
-                  registe_time: '2016-05-02',
-                  username: '王小虎',
-                  city: '上海市普陀区金沙江路 1518 弄'
-                }, {
-                  registe_time: '2016-05-04',
-                  username: '王小虎',
-                  city: '上海市普陀区金沙江路 1517 弄'
-                }, {
-                  registe_time: '2016-05-01',
-                  username: '王小虎',
-                  city: '上海市普陀区金沙江路 1519 弄'
-                }, {
-                  registe_time: '2016-05-03',
-                  username: '王小虎',
-                  city: '上海市普陀区金沙江路 1516 弄'
-                }],
+                tableData: [],
                 currentRow: null,
                 offset: 0,
                 limit: 20,
                 count: 0,
                 currentPage: 1,
+                userName: '',
             }
         },
     	components: {
     		headTop,
     	},
         created(){
-            this.initData();
+            let userName = JSON.parse(sessionStorage.getItem("userName"))
+            this.userName = userName.userName
+            // this.initData();
+            this.JobHistory();
         },
         methods: {
-            async initData(){
-                try{
-                    const countData = await getUserCount();
-                    if (countData.status == 1) {
-                        this.count = countData.count;
-                    }else{
-                        throw new Error('获取数据失败');
-                    }
-                    this.getUsers();
-                }catch(err){
-                    console.log('获取数据失败', err);
-                }
-            },
+            // async initData(){
+            //     try{
+            //         const countData = await getUserCount();
+            //         if (countData.status == 1) {
+            //             this.count = countData.count;
+            //         }else{
+            //             throw new Error('获取数据失败');
+            //         }
+            //         this.getUsers();
+            //     }catch(err){
+            //         console.log('获取数据失败', err);
+            //     }
+            // },
             handleSizeChange(val) {
                 console.log(`每页 ${val} 条`);
             },
             handleCurrentChange(val) {
                 this.currentPage = val;
                 this.offset = (val - 1)*this.limit;
-                this.getUsers()
+                // this.getUsers()
             },
-            async getUsers(){
-                const Users = await getUserList({offset: this.offset, limit: this.limit});
+            async JobHistory(){
+                const jobHistory = await getJobHistory({userName: this.userName});
                 this.tableData = [];
-                Users.forEach(item => {
-                    const tableData = {};
-                    tableData.username = item.username;
-                    tableData.registe_time = item.registe_time;
-                    tableData.city = item.city;
-                    this.tableData.push(tableData);
+                jobHistory['data'].forEach(item => {
+                    const tableDict = {};
+                    tableDict.userName = this.userName;
+                    tableDict.jobContent = item.JobContent;
+                    tableDict.date = item.Date;
+                    this.tableData.push(tableDict);
                 })
+                this.count = this.tableData.length
             }
         },
     }
